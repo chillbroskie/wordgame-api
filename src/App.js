891425ac6671addd26fc5f0d3d2b5184ef939d6a
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2022-01-01 11:24:04 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [chosenLevel, setChosenLevel] = useState(null);
+
+  const getRandomWords = () => {
+    const options = {
+      method: "GET",
+      url: "https://twinword-word-association-quiz.p.rapidapi.com/type1/",
+      params: { level: "3", area: "sat" },
+      headers: {
+        "x-rapidapi-host": "twinword-word-association-quiz.p.rapidapi.com",
+        "x-rapidapi-key": "bdf288e334msh0ab3bad4d8d4932p1359aejsn03ca6e624ce0",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  console.log(chosenLevel);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        name="levels"
+        id="levels"
+        value={chosenLevel}
+        onChange={(e) => setChosenLevel(e.target.value)}
+      >
+        <option value={"1"}>Level 1</option>
+        <option value={"2"}>Level 2</option>
+        <option value={"3"}>Level 3</option>
+      </select>
     </div>
   );
 }
